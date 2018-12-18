6463625ad28ed885f156e9b4c7b89a3baca41cb7
bperquku <bperquku@gmail.com>
2018-12-18 16:12:39 +0100

    Refactored image service to reactive

--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -21,85 +21,88 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class ImageControllerTest {
 
-    @Mock
-    ImageService imageService;
+  @Mock ImageService imageService;
 
-    @Mock
-    RecipeService recipeService;
+  @Mock RecipeService recipeService;
 
-    ImageController controller;
+  ImageController controller;
 
-    MockMvc mockMvc;
+  MockMvc mockMvc;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
 
-        controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller)
-                .setControllerAdvice(new ControllerExceptionHandler())
-                .build();
+    controller = new ImageController(imageService, recipeService);
+    mockMvc =
+        MockMvcBuilders.standaloneSetup(controller)
+            .setControllerAdvice(new ControllerExceptionHandler())
+            .build();
+  }
+
+  @Test
+  public void getImageForm() throws Exception {
+    // given
+    RecipeCommand command = new RecipeCommand();
+    command.setId("1");
+
+    when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
+
+    // when
+    mockMvc
+        .perform(get("/recipe/1/image"))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("recipe"));
+
+    verify(recipeService, times(1)).findCommandById(anyString());
+  }
+
+  @Test
+  public void handleImagePost() throws Exception {
+    MockMultipartFile multipartFile =
+        new MockMultipartFile(
+            "imagefile", "testing.txt", "text/plain", "Spring Framework Guru".getBytes());
+
+    when(imageService.saveImageFile(anyString(), any())).thenReturn(Mono.empty());
+
+    mockMvc
+        .perform(multipart("/recipe/1/image").file(multipartFile))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(header().string("Location", "/recipe/1/show"));
+
+    verify(imageService, times(1)).saveImageFile(anyString(), any());
+  }
+
+  @Test
+  public void renderImageFromDB() throws Exception {
+
+    // given
+    RecipeCommand command = new RecipeCommand();
+    command.setId("1");
+
+    String s = "fake image text";
+    Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+    int i = 0;
+
+    for (byte primByte : s.getBytes()) {
+      bytesBoxed[i++] = primByte;
     }
 
-    @Test
-    public void getImageForm() throws Exception {
-        //given
-        RecipeCommand command = new RecipeCommand();
-        command.setId("1");
+    command.setImage(bytesBoxed);
 
-        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
+    when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
 
-        //when
-        mockMvc.perform(get("/recipe/1/image"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("recipe"));
+    // when
+    MockHttpServletResponse response =
+        mockMvc
+            .perform(get("/recipe/1/recipeimage"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
 
-        verify(recipeService, times(1)).findCommandById(anyString());
+    byte[] reponseBytes = response.getContentAsByteArray();
 
-    }
-
-    @Test
-    public void handleImagePost() throws Exception {
-        MockMultipartFile multipartFile =
-                new MockMultipartFile("imagefile", "testing.txt", "text/plain",
-                        "Spring Framework Guru".getBytes());
-
-        mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", "/recipe/1/show"));
-
-        verify(imageService, times(1)).saveImageFile(anyString(), any());
-    }
-
-
-    @Test
-    public void renderImageFromDB() throws Exception {
-
-        //given
-        RecipeCommand command = new RecipeCommand();
-        command.setId("1");
-
-        String s = "fake image text";
-        Byte[] bytesBoxed = new Byte[s.getBytes().length];
-
-        int i = 0;
-
-        for (byte primByte : s.getBytes()){
-            bytesBoxed[i++] = primByte;
-        }
-
-        command.setImage(bytesBoxed);
-
-        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
-
-        //when
-        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-
-        byte[] reponseBytes = response.getContentAsByteArray();
-
-        assertEquals(s.getBytes().length, reponseBytes.length);
-    }
-
+    assertEquals(s.getBytes().length, reponseBytes.length);
+  }
 }
--- src/test/java/guru/springframework/services/ImageServiceImplTest.java
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -2,6 +2,8 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
+import reactor.core.publisher.Mono;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -19,7 +21,7 @@ import static org.mockito.Mockito.*;
 public class ImageServiceImplTest {
 
     @Mock
-    RecipeRepository recipeRepository;
+    RecipeReactiveRepository recipeReactiveRepository;
 
     ImageService imageService;
 
@@ -27,31 +29,31 @@ public class ImageServiceImplTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        imageService = new ImageServiceImpl(recipeRepository);
+        imageService = new ImageServiceImpl(recipeReactiveRepository);
     }
 
     @Test
     public void saveImageFile() throws Exception {
-        //given
-        String id = "1";
-        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
-                "Spring Framework Guru".getBytes());
+      //given
+      String id = "1";
+      MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+              "Spring Framework Guru".getBytes());
 
-        Recipe recipe = new Recipe();
-        recipe.setId(id);
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
+      Recipe recipe = new Recipe();
+      recipe.setId(id);
 
-        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+      when(recipeReactiveRepository.findById(anyString())).thenReturn(Mono.just(recipe));
+      when(recipeReactiveRepository.save(any(Recipe.class))).thenReturn(Mono.just(recipe));
 
-        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+      ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
-        //when
-        imageService.saveImageFile(id, multipartFile);
+      //when
+      imageService.saveImageFile(id, multipartFile);
 
-        //then
-        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
-        Recipe savedRecipe = argumentCaptor.getValue();
-        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+      //then
+      verify(recipeReactiveRepository, times(1)).save(argumentCaptor.capture());
+      Recipe savedRecipe = argumentCaptor.getValue();
+      assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
     }
 
 }
