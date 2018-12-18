c0922e9078686ce10d194ef57095b959ec125a8f
bperquku <bperquku@gmail.com>
2018-12-18 10:09:00 +0100

    refatored to remove dbrefs

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,16 +1,11 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Set;
-
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
 @Document
@@ -18,7 +13,5 @@ public class Category {
     @Id
     private String id;
     private String description;
-
-    @DBRef
     private Set<Recipe> recipes;
 }
--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,15 +1,10 @@
 package guru.springframework.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-
 import java.math.BigDecimal;
 import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
 public class Ingredient {
@@ -17,8 +12,6 @@ public class Ingredient {
     private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
-
-    @DBRef
     private UnitOfMeasure uom;
 
     public Ingredient() {
