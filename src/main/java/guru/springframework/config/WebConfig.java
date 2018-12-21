85aa54c2344c7e4ea90e52583eba16def6fb6243
bperquku <bperquku@gmail.com>
2018-12-22 00:40:10 +0100

    routerfunctions for recipes

---
+++ src/main/java/guru/springframework/config/WebConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  RouterFunction<?> routes(RecipeService recipeService) {
+    return RouterFunctions.route(
+        RequestPredicates.GET("/api/recipes"),
+        serverRequest ->
+            ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(recipeService.getRecipes(), Recipe.class));
+  }
+}
